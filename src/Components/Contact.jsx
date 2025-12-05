c61e741acb823444f2354cc5febf1c28ae311424
Faezeh <faezehadabi9@gmail.com>
2025-12-05 18:38:03 +0330

    build inputs.map and Form state

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from "react";
 
+const inputs = [
+  { type: "text", name: "Name", placeholder: "Name" },
+  { type: "text", name: "Lastname", placeholder: "Lastname" },
+  { type: "email", name: "Email", placeholder: "Email" },
+  { type: "number", name: "Phone", placeholder: "Phone" },
+];
 function Contact() {
+  const [Form, setForm] = useState({
+    Name: "",
+    Lastname: "",
+    Email: "",
+    Phone: "",
+  });
+
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <form>
+        {inputs.map((input) => (
+          <input
+            type={input.type}
+            name={input.name}
+            placeholder={input.placeholder}
+            value={Form[input.name]}
+          />
+        ))}
+        <button>SUBMIT</button>
+      </form>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
