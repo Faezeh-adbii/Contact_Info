5456cceb6552dadee9df83754ebfdba1f5b6210f
Faezeh <faezehadabi9@gmail.com>
2025-12-06 20:14:30 +0330

    build inputs.js file

---
+++ src/Contants/inputs.js
@@ -0,0 +1,7 @@
+const inputs = [
+    { type: "text", name: "Name", placeholder: "Name" },
+    { type: "text", name: "Lastname", placeholder: "Lastname" },
+    { type: "email", name: "Email", placeholder: "Email" },
+    { type: "number", name: "Phone", placeholder: "Phone" },
+];
+export default inputs
