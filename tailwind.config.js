6ad905a18926416f30c8d9a9a11a7e4400d24185
Faezeh <faezehadabi9@gmail.com>
2025-12-07 20:57:01 +0330

    writing screen items(sm , md , lg ,xl)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
+        screens: {
+            'sm': '570px',
+            'md': '768px',
+            'lg': '992px',
+            'xl': '1200px',
+        },
         extend: {
             gridTemplateColumns: {
-                "New-ContactInfo": 'repeat(auto-fit , minmax(260px , 1fr))'
-            }
+                "New-inputs": 'repeat(auto-fit , minmax(px , 1fr))',
+            },
+
         },
     },
     plugins: [],
