7b0ccfb1f551407cc109861bb266ed0b5bc81cf5
Faezeh <faezehadabi9@gmail.com>
2025-12-14 19:40:50 +0330

    delete consol.log

--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,7 +1,6 @@
 import ContactItem from "./ContactItem";
 
 function ContactList({ Forms }) {
-  console.log("Forms data:", Forms); // برای دیدن ساختار داده
   return (
     <div className="w-[80%] mb-60">
       <h1 className="font-bold text-2xl text-blue-950 mt-14">Contacts List</h1>
